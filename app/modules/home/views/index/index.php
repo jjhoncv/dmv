e509753ebacce7c35be01120e9604185cbec37bf
Jhonnatan Castro <jhonnatan.castro@orbis.com.pe>
2016-12-01 14:51:58 -0500

    valid fb front

--- app/modules/home/views/index/index.php
+++ app/modules/home/views/index/index.php
@@ -1,128 +1,102 @@
-<?php
-require '../app/modules/facebook/facebook.php';
-$facebook = new Facebook(array(
-  'appId'  => '231693037252894',
-  'secret' => '311585a9a89d00da75d86784f525f1fc'
-));
-// See if there is a user from a cookie
-$user = $facebook->getUser();
-if ($user) {
-  try {
-    // Proceed knowing you have a logged in user who's authenticated.
-    $user_profile = $facebook->api('/me');
-  } catch (FacebookApiException $e) {
-    echo '<pre>'.htmlspecialchars(print_r($e, true)).'</pre>';
-    $user = null;
-  }
-}
-?>
-
 <!DOCTYPE html>
-<html xmlns:fb="http://www.facebook.com/2008/fbml">
-  <head>
-    <title>Demo</title>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="description" content="Demo project">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="<?php echo _STATIC_ ?>/libs/dist/bootstrap/dist/css/bootstrap.css">
-    <link rel="stylesheet" href="<?php echo _STATIC_ ?>/css/dist/fonts.css">
-    <link rel="stylesheet" href="<?php echo _STATIC_ ?>/css/dist/index.css">
-    <script type="text/javascript">
-      var yOSON = {
-        "module": "home",
-        "controller": "index",
-        "action": "index",
-        "baseHost": "",
-        "statHost": "",
-        "eHost": "",
-        "statVers": "",
-        "AppCore": {},
-        "AppSandbox": {},
-        "AppSchema": {
-          "modules": {},
-            "requires": {}
-        }
-      };
-    </script>
-  </head>
-  <body>
-    <header>
-      <nav class="navbar navbar-default">
-        <div class="container">
-          <ul class="nav navbar-nav">
-            <li class="active"><a>Login</a></li>
-          </ul>
-        </div>
-      </nav>
-    </header>
-    <main>      
-      <div class="container">
-  
-        <?php if ($user) { ?>
-          Your user profile is
-          <pre>
-            <?php print htmlspecialchars(print_r($user_profile, true)) ?>
-          </pre>
-        <?php } else { ?>
-          <fb:login-button scope="offline_access,publish_stream,sms,email,user_birthday,user_photos,user_photo_video_tags,user_checkins,friends_checkins"></fb:login-button>
-        <?php } ?>
-        <div id="fb-root"></div>
+<html>
+<head>
+<title>Facebook Login JavaScript Example</title>
+<meta charset="UTF-8">
+</head>
+<body>
+<script>
+  // This is called with the results from from FB.getLoginStatus().
+  function statusChangeCallback(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+      // Logged into your app and Facebook.
+      testAPI();
+    } else if (response.status === 'not_authorized') {
+      // The person is logged into Facebook, but not your app.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into this app.';
+    } else {
+      // The person is not logged into Facebook, so we're not sure if
+      // they are logged into this app or not.
+      document.getElementById('status').innerHTML = 'Please log ' +
+        'into Facebook.';
+    }
+  }
 
-        <form>
-          <input type="text" name="username" placeholder="username">
-          <input type="text" name="password" placeholder="password">
-          <input type="submit">
-        </form>
-      </div>
-    </main>
-    <footer>
-      <div class="container">
-        <div class="row">
-          <div class="col-xs-6">
-            <h5>footer</h5>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod</p>
-          </div>
-          <div class="col-xs-6">
-            <h5>Links</h5>
-            <ul>
-              <li class="active"><a>Login</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
-      <div class="footer-copyright">
-        <div class="container">© 2014 Copyright Text<a href="#">More links</a></div>
-      </div>
-    </footer>
+  // This function is called when someone finishes with the Login
+  // Button.  See the onlogin handler attached to it in the sample
+  // code below.
+  function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+  }
 
-    <script>
-      window.fbAsyncInit = function() {
-        FB.init({
-          appId: '<?php echo $facebook->getAppID() ?>',
-          cookie: true,
-          xfbml: true,
-          oauth: true
-        });
-        FB.Event.subscribe('auth.login', function(response) {
-          window.location.reload();
-        });
-        FB.Event.subscribe('auth.logout', function(response) {
-          window.location.reload();
-        });
-      };
+  window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '231693037252894',
+    cookie     : true,  // enable cookies to allow the server to access 
+                        // the session
+    xfbml      : true,  // parse social plugins on this page
+    version    : 'v2.8' // use graph api version 2.8
+  });
 
-      (function() {
-      var e = document.createElement('script'); e.async = true;
-      e.src = document.location.protocol +
-        '//connect.facebook.net/en_US/all.js';
-      document.getElementById('fb-root').appendChild(e);
-      }());
-    </script>
-    
-  </body>
-  <script src="<?php echo _STATIC_ ?>/libs/dist/jquery/dist/jquery.js" type="text/javascript"></script>
-  <script src="<?php echo _STATIC_ ?>/libs/dist/yosonjs/dist/yoson.js" type="text/javascript"></script>
-  <script src="<?php echo _STATIC_ ?>/js/dist/scripts/index.js" type="text/javascript"></script>
-  <script src="<?php echo _STATIC_ ?>/js/dist/libs/yosonjs-utils.js" type="text/javascript"></script>
+  // Now that we've initialized the JavaScript SDK, we call 
+  // FB.getLoginStatus().  This function gets the state of the
+  // person visiting this page and can return one of three states to
+  // the callback you provide.  They can be:
+  //
+  // 1. Logged into your app ('connected')
+  // 2. Logged into Facebook, but not your app ('not_authorized')
+  // 3. Not logged into Facebook and can't tell if they are logged into
+  //    your app or not.
+  //
+  // These three cases are handled in the callback function.
+
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+
+  };
+
+  // Load the SDK asynchronously
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
+  // Here we run a very simple test of the Graph API after login is
+  // successful.  See statusChangeCallback() for when this call is made.
+  function testAPI() {
+    console.log('Welcome!  Fetching your information.... ');
+    FB.api('/me', function(response) {
+      console.log('Successful login for: ' + response.name);
+      document.getElementById('status').innerHTML =
+        'Thanks for logging in, ' + response.name + '!';
+    });
+  }
+</script>
+
+<!--
+  Below we include the Login Button social plugin. This button uses
+  the JavaScript SDK to present a graphical Login button that triggers
+  the FB.login() function when clicked.
+-->
+
+<fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
+</fb:login-button>
+
+<div id="status">
+</div>
+
+</body>
 </html>
