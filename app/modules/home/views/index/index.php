bdbe9807f1678af648701c703d6abe334896300f
Jhonnatan Castro <jhonnatan.castro@orbis.com.pe>
2016-12-01 15:31:59 -0500

    valid fb

--- app/modules/home/views/index/index.php
+++ app/modules/home/views/index/index.php
@@ -87,12 +87,26 @@
   // Here we run a very simple test of the Graph API after login is
   // successful.  See statusChangeCallback() for when this call is made.
   function testAPI() {
-    console.log('Welcome!  Fetching your information.... ');
+    /*console.log('Welcome!  Fetching your information.... ');
     FB.api('/me', function(response) {
       console.log('Successful login for: ' + response.name);
       document.getElementById('status').innerHTML =
         'Thanks for logging in, ' + response.name + '!';
-    });
+    });*/
+    FB.login(
+        function(response) {
+            if (response.authResponse) {
+               console.log('Welcome!  Fetching your information.... ');
+               FB.api('/me', function(response) {
+                   console.log('Good to see you, ' + response.email + '.');
+                   alert('Good to see you, ' + response.email + '.');
+               });
+            } else {
+                console.log('User cancelled login or did not fully authorize.');
+            }
+        },
+        {scope:'email'}
+        );
   }
 </script>
 
