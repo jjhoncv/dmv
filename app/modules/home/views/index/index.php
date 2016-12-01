180c2662b6c2303ccd836c9572b83177dcdbffe3
Jhonnatan Castro <jhonnatan.castro@orbis.com.pe>
2016-12-01 15:20:19 -0500

    valid fb

--- app/modules/home/views/index/index.php
+++ app/modules/home/views/index/index.php
@@ -70,9 +70,18 @@
   //
   // These three cases are handled in the callback function.
 
-  FB.getLoginStatus(function(response) {
+  /*FB.getLoginStatus(function(response) {
     statusChangeCallback(response);
-  });
+  });*/
+    FB.login( function( response ) {
+      if (response.status == 'connected') {
+        document.getElementById('status').innerHTML = 'Thanks for logging in, ' + response.name + '!';
+      }
+    },{
+      scope: 'email,public_profile',
+      return_scopes: true,
+      auth_type: 'rerequest'
+    });
 
   };
 
